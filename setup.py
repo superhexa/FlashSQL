249640597dacf7a74c36b95d6f3034a7c77f498c
Hexa <shexa.developer@gmail.com>
2024-09-08 13:11:09 -0500

    Bug fixes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='FlashSQL',
-    version='0.3.2,
+    version='0.3.2',
     description='A lightweight key-value database using SQLite and APSW.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
